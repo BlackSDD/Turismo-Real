b73531bff514bf015984542b0f448c7ef4a4c2ad
BlackSDD <57774114+BlackSDD@users.noreply.github.com>
2021-10-12 22:25:53 -0300

    turismo proyecto
    se subieron archivos nuevos

---
+++ Turismo/Turismo.Negocio/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Turismo.DALC;
+
+namespace Turismo.Negocio
+{
+    public class Usuario
+    {
+        public decimal Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime UltimaConexion { get; set; }
+
+        TurismoEntities db = new TurismoEntities();
+
+        public bool Autenticar()
+        {
+            return db.USUARIO
+                
+                .Where(u => u.EMAIL == this.Email
+                       && u.PASSWORD == this.Password)
+                
+                .FirstOrDefault() != null;
+
+        }
+
+    }
+}
---
+++ Turismo/Turismo/Controllers/DepartamentoController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Turismo.Negocio;
+
+namespace Turismo.Controllers
+{
+    [Authorize]
+    public class DepartamentoController : Controller
+    {
+        // GET: Departamento
+        public ActionResult Index()
+        {
+            ViewBag.departamentos = new Departamento().ReadAll();
+            return View();
+        }
+
+        // GET: Departamento/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Departamento/Create
+        public ActionResult Create()
+        {
+            EnviarRegiones();
+            return View();
+        }
+
+        private void EnviarRegiones()
+        {
+            ViewBag.region = new Region().ReadAll();
+        }
+
+        // POST: Departamento/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Direccion,Precio,Numero,RegionId")]Departamento departamento)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (!ModelState.IsValid)
+                {
+                    EnviarRegiones();
+                    return View(departamento);
+                }
+                departamento.Save();
+                TempData["mensaje"] = "Se ha creado un nuevo departamento con éxito";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(departamento);
+            }
+        }
+
+       // GET: Departamento/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Departamento d = new Departamento().Find(id);
+
+            if (d == null)
+            {
+                TempData["Mensaje"] = "Departamento no encontrado";
+                return RedirectToAction("Index");
+            }
+            ViewBag.regiones = new Region().ReadAll();
+
+            return View(d);
+        }
+
+        // POST: Departamento/Edit/5
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "Id,Direccion,Precio,Numero,RegionId")]Departamento departamento)
+        {
+            try
+            {
+                if(!ModelState.IsValid)
+                {
+                    EnviarRegiones();
+                    return View(departamento);
+                }
+                // TODO: Add update logic here
+                departamento.Update();
+                TempData["mensaje"] = "Departamento Modificado con éxito";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(departamento);
+            }
+        }
+
+        // GET: Departamento/Delete/5
+        public ActionResult Delete(int id)
+        {
+
+            if (new Departamento().Find(id)==null)
+            {
+                
+                TempData["mensaje"] = "Departamento no encontrado";
+                return RedirectToAction("Index");
+            }
+
+            if(new Departamento().Delete(id))
+            {
+                TempData["mensaje"] = "Departamento Eliminado";
+                return RedirectToAction("Index");
+
+            }
+            TempData["mensaje"] = "No se pudo realizar accion";
+            return RedirectToAction("Index");
+        }
+
+        // POST: Departamento/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
