7833e6d30d40a369b04325591adda61997dd2733
ZNKG <matias76097795@gmail.com>
2021-10-13 21:44:54 -0300

    Update Secuence.sql
    add secuencia marca y orden al código

--- Secuence.sql
+++ Secuence.sql
@@ -1,3 +1,23 @@
+-- Drop sequences
+
+DROP SEQUENCE seq_agencia_externa;
+DROP SEQUENCE seq_articulo;
+DROP SEQUENCE seq_checkin;
+DROP SEQUENCE seq_checkout;
+DROP SEQUENCE seq_comuna;
+DROP SEQUENCE seq_condominio;
+DROP SEQUENCE seq_cont_serv;
+DROP SEQUENCE seq_departamento;
+DROP SEQUENCE seq_gastos;
+DROP SEQUENCE seq_mantencion;
+DROP SEQUENCE seq_marca;
+DROP SEQUENCE seq_region;
+DROP SEQUENCE seq_res_mant;
+DROP SEQUENCE seq_reserva;
+DROP SEQUENCE seq_servextras;
+DROP SEQUENCE seq_tour;
+DROP SEQUENCE seq_transporte;
+
 --Create a new Sequence
 
 CREATE SEQUENCE seq_agencia_externa
@@ -5,139 +25,83 @@ CREATE SEQUENCE seq_agencia_externa
     INCREMENT by 1
     NOCYCLE;
 
-DROP SEQUENCE seq_agencia_externa;
-
-
-
 CREATE SEQUENCE seq_articulo
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
-
-DROP SEQUENCE seq_articulo;
-
-
 
 CREATE SEQUENCE seq_checkin
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
 
-DROP SEQUENCE seq_checkin;
-
-
-
 CREATE SEQUENCE seq_checkout
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
-
-DROP SEQUENCE seq_checkout;
-
-
 
 CREATE SEQUENCE seq_comuna
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
 
-DROP SEQUENCE seq_comuna;
-
-
-
 CREATE SEQUENCE seq_condominio
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
-
-DROP SEQUENCE seq_condominio;
-
-
 
 CREATE SEQUENCE seq_cont_serv
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
 
-DROP SEQUENCE seq_cont_serv;
-
-
-
 CREATE SEQUENCE seq_departamento
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
-
-DROP SEQUENCE seq_departamento;
-
-
 
 CREATE SEQUENCE seq_gastos
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
 
-DROP SEQUENCE seq_gastos;
-
-
-
 CREATE SEQUENCE seq_mantencion
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
 
-DROP SEQUENCE seq_mantencion;
-
-
+CREATE SEQUENCE seq_marca
+    START WITH 1
+    INCREMENT by 1
+    NOCYCLE;
 
 CREATE SEQUENCE seq_region
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
 
-DROP SEQUENCE seq_region;
-
-
-
 CREATE SEQUENCE seq_res_mant
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
-
-DROP SEQUENCE seq_res_mant;
-
-
 
 CREATE SEQUENCE seq_reserva
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
 
-DROP SEQUENCE seq_reserva;
-
-
-
 CREATE SEQUENCE seq_servextras
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
-
-DROP SEQUENCE seq_servextras;
-
-
 
 CREATE SEQUENCE seq_tour
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
 
-DROP SEQUENCE seq_tour;
-
-
-
 CREATE SEQUENCE seq_tranpsorte
     START WITH 1
     INCREMENT by 1
     NOCYCLE;
 
-DROP SEQUENCE seq_transporte;
