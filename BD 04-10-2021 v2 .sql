feadd7c6fc8d989f347ce641695a8b6a78ca3840
ZNKG <matias76097795@gmail.com>
2021-10-19 22:06:37 -0300

    BD version 19/10/2021
    Eliminamos los index, cambiamos la pk de disponibiliad a 2 atributos (id_dpto y la fecha) y bajamos la cantidad de caracteres de las descripciones a 2000

--- BD 04-10-2021 v2 .sql
+++ BD 04-10-2021 v2 .sql
@@ -28,6 +28,8 @@ DROP TABLE mantencion CASCADE CONSTRAINTS;
 
 DROP TABLE marca CASCADE CONSTRAINTS;
 
+DROP TABLE modelo CASCADE CONSTRAINTS;
+
 DROP TABLE region CASCADE CONSTRAINTS;
 
 DROP TABLE res_mant CASCADE CONSTRAINTS;
@@ -58,20 +60,20 @@ CREATE TABLE articulo (
     id_arti              NUMBER(10) NOT NULL,
     nom_arti             NVARCHAR2(100) NOT NULL,
     cant_arti            NUMBER(2),
-    deta_arti            NVARCHAR2(5000) NOT NULL,
+    deta_arti            NVARCHAR2(2000) NOT NULL,
     valor_arti           NUMBER(8) NOT NULL,
     departamento_id_dpto NUMBER(10) NOT NULL
 );
 
 CREATE TABLE checkin (
     id_chi   NUMBER(10) NOT NULL,
-    deta_chi NVARCHAR2(5000) NOT NULL
+    deta_chi NVARCHAR2(2000) NOT NULL
 );
 
 CREATE TABLE checkout (
     id_cho     NUMBER(10) NOT NULL,
     cost_multa NUMBER(8),
-    deta_cho   NVARCHAR2(5000)
+    deta_cho   NVARCHAR2(2000)
 );
 
 CREATE TABLE cliente (
@@ -103,11 +105,11 @@ CREATE TABLE cont_serv (
     id_cont_serv       NUMBER(10) NOT NULL,
     fec_cont           DATE NOT NULL,
     costo_total        NUMBER(7) NOT NULL,
-    descripcion        NVARCHAR2(5000) NOT NULL,
+    descripcion        NVARCHAR2(2000) NOT NULL,
     fec_acord          DATE NOT NULL,
     lugar_recogida     NVARCHAR2(100),
     lugar_destino      NVARCHAR2(100),
-    km_rec             NUMBER(100),
+    km_rec             NUMBER(3),
     reserva_id_rva     NUMBER(10) NOT NULL,
     servextras_id_serv NUMBER(10) NOT NULL
 );
@@ -117,7 +119,7 @@ CREATE TABLE departamento (
     dir_dpto          NVARCHAR2(100) NOT NULL,
     num_dpto          NUMBER(4) NOT NULL,
     n_amb_dpto        NUMBER(1) NOT NULL,
-    desc_dpto         NVARCHAR2(5000) NOT NULL,
+    desc_dpto         NVARCHAR2(2000) NOT NULL,
     costo_arri_dpto   NUMBER(6) NOT NULL,
     img_1_dpto        BLOB NOT NULL,
     img_2_dpto        BLOB,
@@ -143,7 +145,7 @@ CREATE TABLE gastos (
 CREATE TABLE mantencion (
     id_mant           NUMBER(10) NOT NULL,
     cost_mant         NUMBER(8) NOT NULL,
-    deta_mant         NVARCHAR2(5000) NOT NULL,
+    deta_mant         NVARCHAR2(2000) NOT NULL,
     res_mant_id_rmant NUMBER(10) NOT NULL
 );
 
@@ -173,7 +175,7 @@ CREATE TABLE servextras (
     id_serv                    NUMBER(10) NOT NULL,
     nom_serv                   NVARCHAR2(100) NOT NULL,
     tipo_serv                  CHAR(1) DEFAULT 'T' NOT NULL,
-    desc_serv                  NVARCHAR2(5000) NOT NULL,
+    desc_serv                  NVARCHAR2(2000) NOT NULL,
     agencia_externa_id_agencia NUMBER(10) NOT NULL
 );
 
@@ -194,7 +196,7 @@ CREATE TABLE transporte (
     id_serv       NUMBER(10) NOT NULL,
     cost_km_dia   NUMBER(5) NOT NULL,
     cost_km_noc   NUMBER(5) NOT NULL,
-    extra_fest  NUMBER(5) NOT NULL,
+    extra_fest  NUMBER(5) NOT NULL
 );
 
 CREATE TABLE usuario (
@@ -243,34 +245,6 @@ CREATE TABLE marca (
     nombre_marca NVARCHAR2 (100) NOT NULL
 );
 
--- Se crean los index -- 
-
-CREATE UNIQUE INDEX gastos__idx ON
-    gastos (
-        departamento_id_dpto
-    ASC 
-
-);
-
-CREATE UNIQUE INDEX mantencion__idx ON
-    mantencion (
-        res_mant_id_rmant
-    ASC 
-);
-
-CREATE UNIQUE INDEX reserva__idx ON
-    reserva (
-        checkin_id_chi
-    ASC 
-);
-
-CREATE UNIQUE INDEX reserva__idxv1 ON
-    reserva (
-        checkout_id_cho
-    ASC 
-);
-
-
 -- Se modifican las tablas para agregar constraints --
 
 ALTER TABLE agencia_externa ADD CONSTRAINT agencia_externa_pk PRIMARY KEY ( id_agencia );
@@ -310,10 +284,8 @@ ALTER TABLE cont_serv ADD CONSTRAINT cont_serv_pk PRIMARY KEY ( id_cont_serv );
 ALTER TABLE departamento ADD CONSTRAINT departamento_pk PRIMARY KEY ( id_dpto );
 
 ALTER TABLE disponibilidad
-    ADD CONSTRAINT valores_permitidos CHECK ( esta_disp IN ( 'No', 'Si', 'no', 'si' ) 
+    ADD CONSTRAINT val_disp CHECK ( esta_disp IN ( 'No', 'Si', 'no', 'si' ) 
 );
-
-ALTER TABLE disponibilidad ADD CONSTRAINT disponibilidad_pk PRIMARY KEY ( fec_disp );
 
 ALTER TABLE gastos ADD CONSTRAINT gastos_pk PRIMARY KEY ( id_gastos );
 
@@ -326,7 +298,7 @@ ALTER TABLE res_mant ADD CONSTRAINT res_mant_pk PRIMARY KEY ( id_rmant );
 ALTER TABLE reserva ADD CONSTRAINT reserva_pk PRIMARY KEY ( id_rva );
 
 ALTER TABLE servextras
-    ADD CONSTRAINT val_tipo CHECK ( tipo_serv IN ( 'T', 'V' ) 
+    ADD CONSTRAINT val_tipo_serv CHECK ( tipo_serv IN ( 'T', 'V' ) 
 );
 
 ALTER TABLE servextras ADD CONSTRAINT servextras_pk PRIMARY KEY ( id_serv );
@@ -341,11 +313,11 @@ ALTER TABLE tour
 
 ALTER TABLE tour ADD CONSTRAINT tour_pk PRIMARY KEY ( id_serv );
 
-ALTER TABLE transporte
+ALTER TABLE vehiculo
     ADD CONSTRAINT valor_asiento CHECK ( asiento_nigno IN ( 'No', 'Si', 'no', 'si' ) 
 );
 
-ALTER TABLE transporte
+ALTER TABLE vehiculo
     ADD CHECK ( per_silla IN ( 'No', 'Si', 'no', 'si' ) 
 );
 
@@ -362,7 +334,7 @@ ALTER TABLE vehiculo ADD CONSTRAINT vehiculo_pk PRIMARY KEY (patente);
 ALTER TABLE conductor ADD CONSTRAINT conductor_pk PRIMARY KEY (rut_conduc);
 
 ALTER TABLE conductor
-    ADD CONSTRAINT dv CHECK ( dv_conduc IN ( '0', '1', '2', '3', '4',
+    ADD CONSTRAINT dv_conduc CHECK ( dv_conduc IN ( '0', '1', '2', '3', '4',
                                           '5', '6', '7', '8', '9',
                                           'K', 'k' )
 );
@@ -384,8 +356,8 @@ ALTER TABLE vehiculo ADD CONSTRAINT vehiculo_modelo_fk FOREIGN KEY (modelo_id_mo
 ALTER TABLE modelo ADD CONSTRAINT modelo_marca_fk FOREIGN KEY (marca_id_marca)
                         REFERENCES marca ( id_marca);
 
-ALTER conductor ADD CONSTRAINT conductor_transporte_fk FOREIGN KEY (transporte_id_serv)
-                    REFERENCES transporte (id_serv);
+ALTER TABLE conductor ADD CONSTRAINT conductor_transporte_fk FOREIGN KEY (transporte_id_serv)
+                    REFERENCES transporte ( id_serv );
 
 ALTER TABLE agencia_externa
     ADD CONSTRAINT agencia_externa_comuna_fk FOREIGN KEY ( comuna_id_com )
@@ -418,6 +390,8 @@ ALTER TABLE departamento
 ALTER TABLE disponibilidad
     ADD CONSTRAINT disponibilidad_departamento_fk FOREIGN KEY ( departamento_id_dpto )
         REFERENCES departamento ( id_dpto );
+
+ALTER TABLE disponibilidad ADD CONSTRAINT disponibilidad_pk PRIMARY KEY ( fec_disp , departamento_id_dpto );
 
 ALTER TABLE gastos
     ADD CONSTRAINT gastos_departamento_fk FOREIGN KEY ( departamento_id_dpto )
@@ -486,7 +460,6 @@ EXCEPTION
     WHEN OTHERS THEN
         RAISE;
 END;
-/
 
 CREATE OR REPLACE TRIGGER arc_tipo_serv_tour BEFORE
     INSERT OR UPDATE OF id_serv ON tour
@@ -512,6 +485,6 @@ EXCEPTION
         NULL;
     WHEN OTHERS THEN
         RAISE;
-END;/
+END;
 
 commit;
