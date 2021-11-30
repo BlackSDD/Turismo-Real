ddb452a741d6d44d32b6cf1649f981211f5fa0e3
MatiasFuentes-Practicante <matias76097795@gmail.com>
2021-11-30 03:26:52 -0300

    ask get with params

--- Front/Pwa/src/components/Layouts/PaypalCheckout.js
+++ Front/Pwa/src/components/Layouts/PaypalCheckout.js
@@ -1,11 +1,12 @@
-import React, {useState} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import ReactDOM  from 'react-dom';
 import { useHistory} from 'react-router-dom';
+import axios from 'axios';
 
 const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 
-function PaypalCheckout({precio, id_rva}) {
-    const [price, setPrice] = useState(0);
+function PaypalCheckout ({precio, id_rva}) {
+    // const [price, setPrice] = useState(0);
 
     let history = useHistory();
 
@@ -15,7 +16,6 @@ function PaypalCheckout({precio, id_rva}) {
                 amount: {
                 value: precio,
                 },
-
             }]
         });
     };
@@ -24,24 +24,58 @@ function PaypalCheckout({precio, id_rva}) {
         return actions.order.capture(handlePay());
     };
 
+    const id = {id_rva};
+
     function handlePay(e){
-        console.log("Pago recibido")
-        history.push({
-            pathname: '/confirmPago',
-            state: { detail: {id_rva} }
+        console.log("Pago recibido");
+        setCount(true);
+        getInformeRes(id)
+        // history.push({
+        //     pathname: '/confirmPago' + {id_rva},
+        //     state: { detail: {id_rva} }
+        // });
+    }
+    
+    const [informeR, setInformeR] = useState([])
+    const [count, setCount] = useState(false);
+
+    const state = {
+        informereserva: []
+    }
+
+    
+
+    const getInformeRes = async (id) => {
+        const res = await axios.get('http://localhost:4000/API/informeResDet/', { id } )
+        const inR = await JSON.parse(res.data) 
+        setInformeR({inR})
+        this.state({
+            informereserva: res.data
         });
     }
 
-    return (
-        <div>
-            <h1>El monto a pagar es {precio} </h1>
-            <PayPalButton
+    console.log({informeR})
+    console.log({state})
+
+    if(count===false){
+        return( 
+            <div>
+                <h1>El monto a pagar es {precio} </h1>
+                <PayPalButton
                 createOrder={(data, actions) => createOrder(data, actions)}
                 onApprove={(data, actions) => onApprove(data, actions)}
-            />
+                />
+            </div>
+        );
+    }
+    else {
+    return(
+        <div>
+            <h1>Pago recibido</h1>
+            
         </div>
-        
     );
+    }
 }
 
 export default PaypalCheckout  
